1beb9c82800a55e24cd85f3e3949b3c994fae3a6
mac <mac@LAPTOP-8G9S98QT>
2020-07-05 09:14:11 +0800

    控制器

--- src/com/icss/mvc/controller/enterpriseController.java
+++ src/com/icss/mvc/controller/enterpriseController.java
@@ -1,27 +1,40 @@
 package com.icss.mvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.icss.mvc.dao.EnterpriseDao;
+import com.icss.mvc.entity.business_enrol;
+
 @Controller
 public class enterpriseController {
-
+	@Autowired
+	private EnterpriseDao entdao;
+	
+	/* 企业注册 */
 	@RequestMapping("enterpriseSignUp")
 	@ResponseBody
-	public String fune1() {
-		
-		
-		
-		return "ok";
+	public String fune1(business_enrol buen) {
+		System.out.println("Enterprise Sign Up----------");
+		int a=entdao.enterpriseSignup(buen);
+		return "success";
 	}
+	/* 企业登录 */
 	@RequestMapping("enterpriseSignIn")
 	@ResponseBody
-	public String fune2() {
-		
-		
-		
-		return "ok";
+	public String fune2(ModelMap mp,String bsname,String bspsw) {
+		System.out.println("Enterprise Sign In----------");
+		String psw=entdao.enterpriseSignin(bsname);
+		mp.addAttribute("name",bsname);
+		if(bspsw.equals(psw)) {
+			return "success";
+		}
+		else {
+			return "fail";
+		}
 	}
 	
 	
