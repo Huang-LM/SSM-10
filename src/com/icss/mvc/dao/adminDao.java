795e4f41d160c3c5c6b345351bde10da40de47bb
mac <mac@LAPTOP-8G9S98QT>
2020-07-12 21:51:15 +0800

    1

--- src/com/icss/mvc/dao/adminDao.java
+++ src/com/icss/mvc/dao/adminDao.java
@@ -14,14 +14,14 @@ public interface adminDao {
 	/* 管理员登录 */
 	public String adminSignin(String mnname);
 	
-	/* 企业信息展示 */	
+	/* 求职者信息展示 */	
 	public List<jobhunter> findjbhunterAll();
-	/* 企业信息添加 */
+	/* 求职者信息添加 */
 	public int insertjobhunter
 	( String jbname,String jbsex,Integer jbage,String jbid ,String jbusername ,String jbpnmb ,String jbcompany ,String jbjob,String jbablt, String interstatus);
-	/* 企业信息删除 */
+	/* 求职者信息删除 */
 	public int deljbhunter(String jbid);
-	/* 企业信息更改 */
+	/* 求职者信息更改 */
 	public int updatejbhunter
 	(String jbname,String jbsex,Integer jbage,String jbid ,String jbusername ,String jbpnmb ,String jbcompany ,String jbjob,String jbablt, String interstatus);
 	
