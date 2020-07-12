dee6e87651ccb3b8eea3759f772680b8bda1c9c7
mac <mac@LAPTOP-8G9S98QT>
2020-07-12 20:53:22 +0800

    修改

--- src/com/icss/mvc/dao/adminDao.java
+++ src/com/icss/mvc/dao/adminDao.java
@@ -1,5 +1,9 @@
 package com.icss.mvc.dao;
 
+import java.util.List;
+
+import com.icss.mvc.entity.business;
+import com.icss.mvc.entity.jobhunter;
 import com.icss.mvc.entity.manager_enrol;
 
 public interface adminDao {
@@ -9,4 +13,30 @@ public interface adminDao {
 	
 	/* 管理员登录 */
 	public String adminSignin(String mnname);
+	
+	/* 企业信息展示 */	
+	public List<jobhunter> findjbhunterAll();
+	/* 企业信息添加 */
+	public int insertjobhunter
+	( String jbname,String jbsex,Integer jbage,String jbid ,String jbusername ,String jbpnmb ,String jbcompany ,String jbjob,String jbablt, String interstatus);
+	/* 企业信息删除 */
+	public int deljbhunter(String jbid);
+	/* 企业信息更改 */
+	public int updatejbhunter
+	(String jbname,String jbsex,Integer jbage,String jbid ,String jbusername ,String jbpnmb ,String jbcompany ,String jbjob,String jbablt, String interstatus);
+	
+	public List<jobhunter> findjbhunterByid();
+	
+	/* 企业信息展示 */	
+	public List<business> findBusinessAll();
+	/* 企业信息添加 */
+	public int insertBusiness
+	(String bsname,String bspname,String bsid,String bsclass,String bsculture,String bsintr,String bsablt);
+	/* 企业信息删除 */
+	public int delBusiness(String bsid);
+	/* 企业信息更改 */
+	public int updateBusiness
+	(String bsid,String bsname,String bspname,String bsclass,String bsculture,String bsintr,String bsablt);
+	
+	public List<business> findBusinessByid();
 }
